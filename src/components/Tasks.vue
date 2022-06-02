461dded047610a55031b54d19d0c5f47f3bc12d6
Daniel Burrall <Danburrall@gmail.com>
2022-06-02 13:51:23 -0400

    adding more files

---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+<template>
+<div :key="task.id" v-for="task in tasks"> 
+    <Task :task="task" />
+    <h3>{{task.text}} </h3>
+    
+     </div>
+    
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    name:'Tasks',
+    props: {
+        tasks: Array,
+    },
+    components: {
+        Task
+    }
+}
+</script>
